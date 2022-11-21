2973d85c15754916fd7627e286212e57003ccd69
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-21 13:47:41 +0700

    Add error structure

---
+++ src/main/java/com/helpet/exception/util/DefaultEnumLocalizedError.java
@@ -0,0 +1,21 @@
+package com.helpet.exception.util;
+
+public interface DefaultEnumLocalizedError extends LocalizedError {
+    String name();
+
+    default String getCode() {
+        return this.name();
+    }
+
+    default String getExceptionKey() {
+        return this.getClass()
+                   .getSimpleName()
+                   .replace("LocalizedError", "")
+                   .replaceAll("([a-z0-9])([A-Z])", "$1-$2")
+                   .toLowerCase();
+    }
+
+    default String getReasonKey() {
+        return this.name().toLowerCase().replace("_", "-");
+    }
+}
---
+++ src/main/java/com/helpet/exception/util/LocalizedError.java
@@ -0,0 +1,9 @@
+package com.helpet.exception.util;
+
+public interface LocalizedError {
+    String getCode();
+
+    String getExceptionKey();
+
+    String getReasonKey();
+}
